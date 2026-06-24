c1bda846916fb48b8fbdc676da7a123797b766c9
ysdlzmm <dx_0729@163.com>
2026-06-24 17:10:16 +0800

    chore: 添加commitlint和husky配置
    - 配置commitlint强制Conventional Commits规范
    - husky在commit-msg阶段验证提交信息
    - 不符合规范的commit将被拒绝

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // 新功能
+        'fix',      // 修复bug
+        'docs',     // 文档
+        'style',    // 格式
+        'refactor', // 重构
+        'perf',     // 性能
+        'test',     // 测试
+        'chore',    // 构建/工具
+        'ci',       // CI
+        'revert',   // 回滚
+      ],
+    ],
+    'subject-max-length': [2, 'always', 100],
+  },
+};
